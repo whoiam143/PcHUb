ea4bc0ed108de33ba6f7dd9b68bf329314ee1433
zetistheself <qweasdzxc0091@mail.ru>
2024-03-22 22:30:38 +0300

    Added [requirements.txt, /data/__all_models.py, /data/db_session.py, /db/data.py, /forms, /templates/base.html, .gitgnore, requirements.txt]

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from flask import Flask, url_for, request, render_template, redirect
+from data import db_session
+from flask_login import login_user, LoginManager, login_required, logout_user
 
+
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+def main():
+    db_session.global_init("db/data.sqlite3")
+    app.run(port=8080, host='127.0.0.1')
+
+
+@app.route('/')
+def index():
+    return render_template('base.html', title='Главная страница')
+
+
+
+
+if __name__ == "__main__":
+    main()
